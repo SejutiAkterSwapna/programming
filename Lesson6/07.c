85abeebb8a25726a0992c2c6bb4ead2243cdfbfc
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2025-04-29 21:00:46 +0600

    learning array

--- Lesson6/07.c
+++ Lesson6/07.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int ara[5]={10,20,30,40,50}; 
+    int ara[5]={10,20,30,40,50,76}; 
     
     printf("First element: %d\n", ara[0]);
     printf("Third element: %d\n", ara[2]);
     printf("forth element: %d\n", ara[3]);
     printf("forth element: %d\n", ara[4]);
+    printf("forth element: %d\n", ara[5]);
     return 0;
 }
