5fd464a7b95f9df23062a17189b3014fee2ea127
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2025-04-29 18:30:22 +0600

    learning ARA

---
+++ Lesson6/07.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int ara[5]={10,20,30,40,50}; 
+    
+    printf("First element: %d\n", ara[0]);
+    printf("Third element: %d\n", ara[2]);
+    printf("forth element: %d\n", ara[3]);
+    return 0;
+}
