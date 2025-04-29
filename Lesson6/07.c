98f315365f648912ac18e8ff0c792477258643f5
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2025-04-29 21:11:50 +0600

    Signed commit test

--- Lesson6/07.c
+++ Lesson6/07.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int ara[5]={10,20,30,40,50,76}; 
+    int ara[5]={10,20,30,40,50,76,80}; 
     
     printf("First element: %d\n", ara[0]);
     printf("Third element: %d\n", ara[2]);
     printf("forth element: %d\n", ara[3]);
     printf("forth element: %d\n", ara[4]);
     printf("forth element: %d\n", ara[5]);
+    printf("forth element: %d\n", ara[6]);
     return 0;
 }
