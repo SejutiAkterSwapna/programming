0db2295f81e4e07b102aad617ec63ecc12fcb735
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2025-05-02 22:26:27 +0600

    area of radius

---
+++ Lesson2/026.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//area of radius
+int main(){
+    float radius;
+    printf("Enter radius:");
+    scanf("%f",&radius);
+
+    printf("Areas is: %0.2f",3.14*radius*radius);
+    return 0;
+}
