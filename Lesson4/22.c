e9849a592d0e9cb758e5a173bc54e97903e2d8cd
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2025-05-12 09:49:53 +0600

    for loop

--- Lesson4/22.c
+++ Lesson4/22.c
@@ -5,13 +5,14 @@ int main()
     int n,sum=0; 
     printf("inter number:");
     scanf("%d",&n);
-    for(int i=1 ; i<=n; i++){
+    for(int i=1,j=n; i<=n && j>=1; i++, j--){
        sum=sum+i;
+       printf("%d\n",j);
     }
     printf("Sum is:%d\n",sum);
 
-    for(int i=n ; i>=1; i--){
-        printf("%d\n",i);
-    }
+    // for(int i=n ; i>=1; i--){
+    //     printf("%d\n",i);
+    // }
     return 0;
 }
