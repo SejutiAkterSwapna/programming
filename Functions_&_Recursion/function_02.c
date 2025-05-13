7fc13cf4b4a05c72823f05fef017e20ba6a30df8
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2025-05-13 16:37:13 +0600

    learn functino

---
+++ Functions_&_Recursion/function_02.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void printHello();
+void print_goodBye();
+int main(){
+    printHello();
+    print_goodBye();
+
+    return 0;
+}
+
+void printHello(){
+    printf("Hello!\n");
+}
+void print_goodBye(){
+    printf("Good bye\n");
+}   
