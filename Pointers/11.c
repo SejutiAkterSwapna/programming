776fa8e8d26e28e89cbc7925fb0e3afb4a30cb91
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2025-05-18 17:55:02 +0600

    calculate the sum, product & average with function of pointer

---
+++ Pointers/11.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void do_work(int a, int b, int *sum, int *prod, int *avg){
+    *sum = a+b;
+    *prod = a*b;
+    *avg = (a+b)/2;
+} 
+int main(){
+    int a = 3, b = 5;
+    int sum, prod, avg;
+    do_work(a,b,&sum,&prod,&avg);
+
+    printf("sum=%d, prod=%d, avg=%d\n",sum,prod,avg);
+    return 0;
+}
