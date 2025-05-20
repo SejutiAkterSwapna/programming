029eefd2e198e339662dbe85c47da38098e377ac
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2025-05-20 18:12:58 +0600

    learn string

---
+++ string/01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (){
+    char name[]={'s','e','j','u','t','i','\0'};
+    char _name[]="SEJUTI AKTER SWAPNA";
+    printf("%s\n",name);
+    printf("%s\n",_name);
+    return 0;
+}
