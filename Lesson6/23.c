38303a4ce5ed7566f1505b0d70892e97c64d157d
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2025-05-20 09:27:51 +0600

    array is apointer

---
+++ Lesson6/23.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+   int ID [5];
+   
+   //inout
+   int* ptr=&ID[0];
+   for(int i=0; i<5; i++){
+    printf("%d index: ",i);
+    scanf("%d",(ptr+i));
+   }
+   printf("------------\n");
+   //output
+   for(int i=0; i<5; i++){
+    printf("%d index:%d\n",i,*(ptr+i));
+   }
+   return 0;
+}
