3c44704cefa324c894e73dee36cb5eb696bb1928
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2025-05-22 10:52:56 +0600

    Array is a pointer arithmetic

--- Array/25.c
+++ Array/25.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-int main ()
+int main()
 {
-    
-    return 0;
-}
+   int ID [5];
+   
+   //inout
+   int* ptr=&ID[0];
+   for(int i=0; i<5; i++){
+    printf("%d index: ",1+i);
+    scanf("%d",&ID[i]);
+   }
+   printf("------------\n");
+   //output
+   for(int i=0; i<5; i++){
+    printf("%d index:%d\n",1+i,ID[i]);
+   }
+   return 0;
+} 
