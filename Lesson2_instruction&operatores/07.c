2474150a6e2ef31a101883c0fff5b8e3f8af7ccf
Sejuti Akter Swapna <sejutiakterswapna@gmail.com>
2025-06-16 08:19:29 -0600

    Average of 3 number

---
+++ Lesson2_instruction&operatores/07.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int num1 = 6;
+    int num2 = 10;
+    int num3 = 15;
+    float average = num1+num2+num3;
+
+    printf("Average number is:%0.2f\n",average/3);
+    return 0;
+}
